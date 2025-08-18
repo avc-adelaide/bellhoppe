28868c106b3d21bb1805b932960fa48f22a60dba
Will Robertson <wspr81@gmail.com>
2025-08-18 21:32:30 +0930

    remove some non-bellhop code from a test

--- tests/arctic/runtests.m
+++ tests/arctic/runtests.m
@@ -12,15 +12,3 @@ caxisrev( [ 50 100 ] )
 bellhop( 'arcticB_gb' )
 plotshd( 'arcticB_gb.shd', 2, 2, 2 )
 caxisrev( [ 50 100 ] )
-
-kraken( 'arcticK' )
-plotshd( 'arcticK.shd.mat', 2, 2, 3 )
-caxisrev( [ 50 100 ] )
-
-scooter( 'arcticS' )
-plotshd( 'arcticS.shd.mat', 2, 2, 4 )
-caxisrev( [ 50 100 ] )
-
-simplePE arcticB
-plotshd( 'arcticB.shd.mat', 2, 2, 4 )
-caxisrev( [ 50 100 ] )
--- tests/arctic/runtestsM.m
+++ tests/arctic/runtestsM.m
@@ -8,11 +8,3 @@ caxisrev( [ 50 100 ] )
 bellhopM( 'arcticB_gb' )
 plotshd( 'arcticB_gb.shd.mat', 2, 2, 2 )
 caxisrev( [ 50 100 ] )
-
-kraken( 'arcticK' )
-plotshd( 'arcticK.shd.mat', 2, 2, 3 )
-caxisrev( [ 50 100 ] )
-
-scooterM( 'arcticS' )
-plotshd( 'arcticS.shd.mat', 2, 2, 4 )
-caxisrev( [ 50 100 ] )
