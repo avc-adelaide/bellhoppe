ca78093ab26c7bdca0c42b167b242cffae062f80
Will Robertson <wspr81@gmail.com>
2025-09-14 10:12:58 +0930

    quieten this test

--- tests/MunkB_geo_rot/test_MunkB_geo_rot.py
+++ tests/MunkB_geo_rot/test_MunkB_geo_rot.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 def test_MunkB_geo_rot_A():
-    """Test from Bellhop examples to calculate arrival times. Just check that there are no execution errors.
+    """Test using a Bellhop example that ENV file parameters are being picked up properly.
+    Just check that there are no execution errors.
     """
 
     env = bh.read_env2d("tests/MunkB_geo_rot/MunkB_geo_rot.env")
@@ -15,7 +16,7 @@ def test_MunkB_geo_rot_A():
     env["soundspeed"] = ssp
     env["depth"] = bty
 
-    assert ssp.shape[1] == 30, "Should be N=30 SSP data points."
+    assert ssp.shape[1] == 30, "Should be N=30 SSP data points"
     assert env['soundspeed_interp'] == 'quadrilateral', "SSPOPT = 'QVF' => Q == quadrilateral"
     assert env['top_boundary_condition'] == 'vacuum', "SSPOPT = 'QVF' => V == vacuum"
     assert env['attenuation_units'] == 'frequency dependent',  "SSPOPT = 'QVF' => F == frequency dependent"
@@ -26,11 +27,10 @@ def test_MunkB_geo_rot_A():
     assert env['box_depth'] == 99500.0, "0.0  99500.0  5.0		! STEP (m), ZBOX (m), RBOX (km)"
     assert env['box_range'] == 5000.0, "0.0  99500.0  5.0		! STEP (m), ZBOX (m), RBOX (km)"
 
-    print(env)
     bh.check_env2d(env)
-    bh.print_env(env)
+    # bh.print_env(env)
 
     tl = bh.compute_transmission_loss(env,debug=True)
-    assert tl is not None, "No results generated."
+    assert tl is not None, "No results generated"
 
-    print(tl)
+    #print(tl)
