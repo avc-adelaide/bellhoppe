3f08b3a32f21d094dd3b6ccbd60d830f858c4c5c
Will Robertson <wspr81@gmail.com>
2025-10-04 18:26:34 +0930

    add test for models()

---
+++ tests/test_models.py
@@ -0,0 +1,38 @@
+import pytest
+import bellhop as bh
+import numpy as np
+
+def test_models():
+
+    env = bh.create_env2d()
+    models = bh.models()
+    print(models)
+    assert models is not None
+
+def test_models_arg():
+
+    env = bh.create_env2d()
+    with pytest.raises(ValueError,match="env and task should be both specified together"):
+        models = bh.models(env)
+
+def test_models_arg_task():
+
+    env = bh.create_env2d()
+    with pytest.raises(ValueError,match="env and task should be both specified together"):
+        models = bh.models(task="foobar")
+
+def test_models_task():
+
+    env = bh.create_env2d()
+    models = bh.models(env,"coherent")
+    print(models)
+    assert models is not None
+
+
+def test_models_task():
+    """I would expect this to error but it doesn't :)"""
+    env = bh.create_env2d()
+    models = bh.models(env,"foobar")
+    print("foobar model?")
+    print(models)
+    assert models is not None
