2d1d61d56e28446f1507bceba4d411093dfdc75f
Will Robertson <wspr81@gmail.com>
2025-10-06 10:42:17 +1030

    add test with explicit models

---
+++ tests/test_01_simple_model.py
@@ -0,0 +1,52 @@
+import pytest
+import bellhop as bh
+
+
+
+def test_arrivals():
+    """Test with default settings to calculate arrival times. Just check that there are no execution errors.
+    """
+
+    env = bh.create_env2d()
+    arr = bh.compute_arrivals(env, model="bellhop")
+    #print(arr)
+
+def test_arrivals():
+    """Test with default settings to calculate arrival times. Catch error for unknown model.
+    """
+
+    with pytest.raises(ValueError, match=r"Unknown model"):
+        env = bh.create_env2d()
+        arr = bh.compute_arrivals(env, model="bellhop_not_found")
+
+
+
+def test_eigenrays():
+    """Test with default settings to calculate eigenrays. Just check that there are no execution errors.
+    """
+
+    env = bh.create_env2d()
+    rays = bh.compute_eigenrays(env, model="bellhop")
+    #print(rays)
+
+
+
+def test_rays():
+    """Test with default settings to calculate rays. Just check that there are no execution errors.
+    """
+
+    env = bh.create_env2d()
+    rays = bh.compute_rays(env, model="bellhop")
+    #print(rays)
+
+
+
+def test_tl():
+    """Test with default settings to calculate transmission loss. Just check that there are no execution errors.
+    """
+
+    env = bh.create_env2d()
+    tl = bh.compute_transmission_loss(env, model="bellhop")
+    #print(tl)
+
+
